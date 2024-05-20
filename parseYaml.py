2052b8c7abf8aa48a70eefa7516650931b9a0c71
Mohammed HARROUD <48032718+MedIAIA@users.noreply.github.com>
2024-05-20 22:12:47 +0200

    Create deploy

---
+++ parseYaml.py
@@ -0,0 +1,11 @@
+
+
+
+def is__bool(j , i):
+     # Check if j is 1, 1, or j+i is 1
+    if j == 1 or j==1 or j+i == 1:
+        return True
+    else:
+        return False
+
+print(is__bool(5 , -8))
